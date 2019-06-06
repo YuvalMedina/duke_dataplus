b6914d0f582dbfd612979470914693ecad7d367e
jin cho <jcho.1260@gmail.com>
2019-06-06 12:11:36 -0400

    added a helper function that finds the gaps in data

--- gap_fill_jin.py
+++ gap_fill_jin.py
@@ -5,21 +5,53 @@ import matplotlib.dates as pltdates
 import datetime as dt
 from main_functions import readFile, getData, plotGraph
 
-def daily_avg_sqDiff(DataForm):
+def find_gap(DataForm):
+    #takes in DataFrame with gaps for one region and siteID
+    #returns dictionary with variables as keys containing start and end dates of gaps
+        #if only one day of missing value, start and end date are the same
+    variables = DataForm.variable.unique()
+    gap_var = {}
+    for v in variables:
+        i=0
+        values = DataForm.loc[DataForm['variable']==v]
+        while(!values['value'].iloc[i].isnull()):
+            i++
+        start_date = values['dateTimeUTC'].iloc[i]
+        i++
+        if(values['value'].iloc[i].isnull()):
+            while(values['value'].iloc[i].isnull()):
+                i++
+            end_date = values['dateTimeUTC'].iloc[i]
+        else:
+            gap_var[v] = [start_date, start_date]
+        gap_var[v] = [start_data, end_date]
+    return gap_var
+
+
+def daily_stand_avg(DataForm, start_date, end_date):
     #takes in a DataFrame containing all information for one siteID, returns dictionary of dataframes containing difference in averages between consecutive days
     DataForm['month'] = pd.DatetimeIndex(DataForm['dateTimeUTC']).month
     DataForm['day'] = pd.DatetimeIndex(DataForm['dateTimeUTC']).day
-    variables = az_lv.variable.unique()
+    start_hour = start_date.hours
+    start_minute = start_date.minutes
+    no_gap = pd.DataFrame()
+    i=0
+    while(DataForm['dateTimeUTC'].iloc[i].hours != start_hour & DataForm['dateTimeUTC'].iloc[i].minutes != start_minute):
+        no_gap.append(DataForm.iloc[i])     #appends data before gap starts to no_gap dataframe
+    while(DataForm['dateTimeUTC'].iloc[i]>=)
+    variables = DataForm.variable.unique()
     byVar = {}
     for v in variables:
         values = DataForm.loc[DataForm['variable']==v]
         #values = values.groupby(['month','day']).mean()
-        values = values.set_index(['regionID', 'siteID', 'dateTimeUTC', 'variable', 'flagID', 'flagComment', 'month', 'day']).groupby(['month', 'day']).apply(filter_vals).reset_index().dropna()
+        values = values.set_index(['regionID', 'siteID', 'dateTimeUTC', 'variable', 'flagID', 'flagComment', 'month', 'day']).groupby(['month', 'day']).apply(filter_vals).reset_index()
         values = values.drop(['month', 'day'], axis=1)
-        diff_byDay = pd.DataFrame()
-        diff_byDay = values.diff()
-        sq_diff_byDay = np.square(diff_byDay)
-        byVar[v] = sq_diff_byDay
+        values['value'] = (values.value - values.value.mean()) / values.value.std()
+        #diff_byDay = pd.DataFrame()
+        #diff_byDay = values.diff()
+        #sq_diff_byDay = np.square(diff_byDay)
+        #byVar[v] = sq_diff_byDay
+        byVar[v] = values
     return byVar
 
 def filter_vals(x):
@@ -45,7 +77,7 @@ def getDataSite(DataForm, region, site):
 
 myfile = readFile();
 DataForm = getDataSite(myfile, 'AZ', 'LV')
-print(daily_avg_sqDiff(DataForm))
+print(daily_stand_avg(DataForm))
 
 DataForm['month'] = pd.DatetimeIndex(DataForm['dateTimeUTC']).month
 DataForm['day'] = pd.DatetimeIndex(DataForm['dateTimeUTC']).day
