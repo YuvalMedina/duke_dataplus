5742fae9b62a68d0b94cb2cdab42566446abc869
YuvalMedina <yuval.medina@duke.edu>
2019-06-06 14:19:27 -0400

    made some important changes to main_functions: now parameters are optional for both getData and plotGraph, and can be specified in any order

--- gap_filler_Yuval.py
+++ gap_filler_Yuval.py
@@ -24,10 +24,12 @@ def gapFill(DataForm, start_date, end_date, gapvariable):
     averages = dict()       #dictionary of averages for each variable and date
     myaverage = dict()      #dictionary of averages for each variable in OUR date (the gap)
     while(DataForm['dateTimeUTC'].iloc[i] < DataForm['dateTimeUTC'].iloc[-1] - duration):       #index -1 means the last index in the dataform
-        if(DataForm['dateTimeUTC'].iloc[i] == start_date):
+        if(DataForm['dateTimeUTC'].iloc[i].date() == start_date):
             j = 0
             while(DataForm['dateTimeUTC'].iloc[i] <= end_date):         #loop through to get the gap average
                 myaverage[DataForm['variable'].iloc[i]] = (myaverage[DataForm['variable'].iloc[i]] * j + DataForm['value'].iloc[i]) / (j+1)
+                print(myaverage[DataForm['variable'].iloc[i]])
+                print('got here 1')
                 i += 1
                 j += 1
         else:
@@ -35,10 +37,11 @@ def gapFill(DataForm, start_date, end_date, gapvariable):
             j = 0
             while(DataForm['dateTimeUTC'].iloc[i] < dictindex + duration):      #while our current date is within our current period
                 currentdate = DataForm['dateTimeUTC'].iloc[i]
-                while(DataForm['dateTimeUTC'].iloc[i] == currentdate):          #loop through one set of variables for one specific timestamp (currentdate)
+                while(DataForm['dateTimeUTC'].iloc[i].date() == currentdate):          #loop through one set of variables for one specific timestamp (currentdate)
                     if(DataForm['variable'].iloc[i] != gapvariable):
                         key = [dictindex, DataForm['variable'].iloc[i]]           #a tuple containing the starting date, and the variable for one entry in the averages dictionary
                         averages[key] = (averages[key] * j + DataForm['value'].iloc[i]) / (j+1)     #update the average
+                        print(averages[key])
                     i += 1
                 j += 1         #j is the number of elements we count for each variable (we increment it after looping through one complete set of different variables for one timestamp)
     squared_differences = dict()
@@ -50,14 +53,17 @@ def gapFill(DataForm, start_date, end_date, gapvariable):
         variable_stddevs[key] = np.std(this_variable['value'])
     for key in averages:
         squared_differences[key[0]] += (averages[key] - myaverage[key[1]])**2 / variable_stddevs[key[1]]        #calculate standardized squared_differences
+    print(averages)
+    print(myaverage)
+    print(squared_differences)
     closest_date = min(squared_differences, key = squared_differences.get)
     fill_with = DataForm.loc[(DataForm['dateTimeUTC'] >= closest_date) & (DataForm['dateTimeUTC'] < closest_date + duration) & (DataForm['variable']==gapvariable)]
     fill_with['dateTimeUTC'].add(dt.timedelta(start_date) - dt.timedelta(closest_date))
     DataForm.append(fill_with, ignore_index=True)
     return DataForm
 
-riverdata = getData(myfile, 'AZ', 'LV', 'WaterTemp_C')
-plotGraph(riverdata)
+riverdata = getData(myfile, region='AZ', site='LV')
+plotGraph(riverdata, variable='WaterTemp_C')
 
-filled = gapFill(riverdata, dt.datetime(2018,6,7),dt.datetime(2018,7,11),'WaterTemp_C')
-plotGraph(filled)
+filled = gapFill(riverdata, dt.datetime(2018,6,7,15,0),dt.datetime(2018,7,11,19,45),'WaterTemp_C')
+plotGraph(filled, 'WaterTemp_C')
