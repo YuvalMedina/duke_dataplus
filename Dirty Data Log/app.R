6442a31c576be3eecb23de261c22a8b2d67d5059
jin cho <jcho.1260@gmail.com>
2019-06-21 11:33:20 -0400

    created a gap detector to use in app

--- Dirty Data Log/app.R
+++ Dirty Data Log/app.R
@@ -1,9 +1,16 @@
 library(shiny)
 
+bigDataSet <- read.csv("NC_NHC_DO_mgL.csv")
+
 ui <- fluidPage(
  navbarPage(title = "Guide to Dirty Data",
             tabPanel("Metabolism", 
               h1("Metabolism"),
+              br(),
+              p("Stream metabolism can be thought of as the exchange of energy in the forms of oxygen and carbon. 
+                This exchange occurs within the stream scosystem between autotrophs and heterotrophs in addition to with the external ecosystem."),
+              HTML('<center><img src="O and CO2 Exchange.jpg", width = 500, height = 300></center>'),
+              br(),
               p("In 1956, Odum and his team developed a model that estimates metabolism as a function of oxygen concentration:"),
               withMathJax(
                 helpText('$$\\frac{dO}{dt}=\\mathrm{GPP+ER}+K*\\mathrm{O}_\\mathrm{def}$$')
@@ -19,7 +26,9 @@ ui <- fluidPage(
                    )
                  ),
                  mainPanel(
-                   h1("Examples")
+                   h1("Examples"),
+                   textOutput("selected_clean"),
+                   plotOutput("plot_time")
                  )
                )      
             ),
@@ -41,7 +50,37 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-
+  clean_type <- reactive({(input$cleanVar)})
+  output$selected_clean <- renderText({ 
+    paste("Below is an oxygen curve considered to be clean and complete for the time period of a", tolower(clean_type()), ":")
+  })
+  output$plot_time <- renderPlot({
+    if(clean_type() == "Day"){
+      clean <- read.csv("Day.csv")
+      graph_type <- "Day of Dissolved Oxygen Data"
+    }
+    if(clean_type() == "Week"){
+      clean <- read.csv("Week.csv")
+      graph_type <- "Week of Dissolved Oxygen Data"
+    }
+    if(clean_type() == "Month"){
+      clean <- read.csv("Month.csv")
+      graph_type <- "Month of Dissolved Oxygen Data"
+    }
+    if(clean_type() == "Year"){
+      clean <- read.csv("Month.csv")
+      graph_type <- "Year of Dissolved Oxygen Data"
+    }
+    df <- data.frame(
+      date = clean$dateTimeUTC,
+      value = clean$value
+    )
+    plot(df$date, df$value,
+         type='l',
+         main = graph_type, 
+         xlab ="Time Stamp", 
+         ylab ="DO_mgL")
+  })
   
 }
 
