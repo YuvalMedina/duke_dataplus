8170d1beb08c170f884e61e45cd6901619c7339d
jin cho <jcho.1260@gmail.com>
2019-07-10 15:37:32 -0400

    created outlier detector and working on plotting

--- outlier_detect_jin.R
+++ outlier_detect_jin.R
@@ -1,19 +1,30 @@
+library(plyr)
+library(ggplot2)
+
 outlier.detect <- function(raw_dat){
-  #finds the index of where all the gaps end
   
   my_dat <- read.csv(raw_dat)   #read csv of one site, one variable
   #new dataframe of dates and values
   df <- data.frame(date = as.POSIXct(my_dat$dateTimeUTC, tz='', 
                                      format = "%Y-%m-%d %H:%M:%S"), value = my_dat$value)   
-  #new "gap" column with time intervals
-  df$gap <- c(NA, with(df, date[-1] - date[-nrow(df)]))
-  #what the gap should be
-  gap_threshold <- gap_time 
-  #see if gap is larger than threshold
-  df$over_thresh <- df$gap > gap_threshold
-  #list of TRUE or FALSE for if gap>threshold
-  gaps <- df$over_thresh == TRUE
-  #list "where_gaps" of indices of end of gap
-  where_gaps = which(gaps)
-  where_gaps
+  #new "diff" column with consecutive value differences
+  df$diff <- c(NA, with(df, abs(value[-1] - value[-nrow(df)])))
+  #what the biggest difference should be
+  diff_threshold <- 1
+  #see if diff is larger than threshold
+  df$over_thresh <- df$diff > diff_threshold
+  #indices of there difference is over threshold
+  where_out = which(df$over_thresh)
+  outliers <- ldply(where_out, function(x) my_dat[x,])
+  outliers
 }
+
+my_out <- outlier.detect("NC_Eno_DO_mgL.csv")
+my_dat <- read.csv("NC_Eno_DO_mgL.csv")
+
+ggplot()+
+  geom_point(my_dat, mapping=aes(x=dateTimeUTC, y=value, colour="blue"))+
+  geom_point(my_out, mapping=aes(x=dateTimeUTC, y=value, colour="red"))
+
+
+    
