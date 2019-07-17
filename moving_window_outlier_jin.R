399fffdf1abf6650bfbc89d37495482b9fcc1c62
jin cho <jcho.1260@gmail.com>
2019-07-17 10:59:46 -0400

    some changes to app. using rollapply to make moving window for outlier

---
+++ moving_window_outlier_jin.R
@@ -0,0 +1,149 @@
+library(zoo)
+
+z <- zoo(11:15, as.Date(31:35))
+rollapply(z, 2, mean)
+
+## non-overlapping means
+z2 <- zoo(rnorm(6))
+rollapply(z2, 3, mean, by = 3)      # means of nonoverlapping groups of 3
+aggregate(z2, c(3,3,3,6,6,6), mean) # same
+
+## optimized vs. customized versions
+rollapply(z2, 3, mean)   # uses rollmean which is optimized for mean
+rollmean(z2, 3)          # same
+rollapply(z2, 3, (mean)) # does not use rollmean
+
+DF <- data.frame(a = 1:10, b = 21:30, c = letters[1:10])
+replace(DF, 1:2, rollapply(DF[1:2], 3, sum, fill = NA))
+
+
+
+library(dplyr)
+library(ggplot2)
+library(ks)
+library(sp)
+library(plyr)
+library(zoo)
+
+#function to remove duplicate data
+remove_dupe <- function(raw_dat){
+  is_dupe = duplicated(raw_dat$dateTimeUTC) |
+    duplicated(raw_dat$dateTimeUTC, fromLast=TRUE)
+  dupes = raw_dat[is_dupe,]
+  dupe_key = factor(paste(dupes$dateTimeUTC, dupes$value))
+  keepers = data.frame()
+  for(k in levels(dupe_key)){
+    dupe_group_inds = which(dupe_key == k)
+    dupe_group = dupes$flagID[dupe_group_inds]
+    keep = dupe_group_inds[dupe_group == 'Bad Data'][1]
+    if(is.na(keep)) keep = dupe_group_inds[1]
+    keepers = rbind(keepers, dupes[keep,])
+  }
+  raw_dat = filter(raw_dat, ! is_dupe) %>%
+    bind_rows(keepers) %>%
+    arrange(dateTimeUTC)
+  raw_dat
+}
+
+#function to fill missing time stamps with NA
+fill_time <- function(raw_dat){
+  time_interval = as.numeric(difftime(raw_dat$dateTimeUTC[2],
+                                      raw_dat$dateTimeUTC[1]))
+  full_dat = seq.POSIXt(raw_dat$dateTimeUTC[1],
+                        raw_dat$dateTimeUTC[nrow(raw_dat)], by=paste(time_interval, 'min'))
+  filled = full_join(data.frame(dateTimeUTC=full_dat), raw_dat)
+  filled
+}
+
+#remove rows with NA for any variables
+remove_na <- function(raw_dat){
+  raw_dat[complete.cases(raw_dat),]
+}
+
+#function to properly format df input for cor_coeff functions
+prep_corr_dat <- function(var1_df, var2_df){
+  var1_df$dateTimeUTC <- as.POSIXct(var1_dfdateTimeUTC, tz='UTC')
+  var2_df$dateTimeUTC <- as.POSIXct(var2_df$dateTimeUTC, tz='UTC')
+  #remove duplicates then fill with NA
+  var1 <- fill_time(remove_dupe(var1_df))
+  var2 <- fill_time(remove_dupe(var2_df))
+  corr_dat <- merge(var1,var2,by="dateTimeUTC")
+  corr_dat <- data.frame(date=corr_dat$dateTimeUTC,
+                         var1 = corr_dat$value.x,
+                         var2 = corr_dat$value.y)
+  corr_dat
+}
+
+cor_coef_singleCluster <- function(corr_dat, var1, var2){
+  #corr_dat <- dataframe with date, var1, var2 columns
+  #returns list of timestamps for outliers
+  #remove rows with NA
+  corr_dat <- corr_dat[complete.cases(corr_dat),]
+  #new df with no dates
+  corr_dat_noDate <- data.frame(var1 = corr_dat$var1, 
+                                var2 = corr_dat$var2)
+  corr_mat <- data.matrix(corr_dat_noDate)
+  #get KDE
+  fhat <- kde(x=corr_mat)
+  #points making 95% contour line
+  contour.5 <- with(fhat,contourLines(x=eval.points[[1]],y=eval.points[[2]],
+                                      z=estimate,levels=cont["5%"])[[1]])
+  contour.5 <- contour.5[-1]
+  #finding points outside of 95 line (have value 0)
+  in_95 <- point.in.polygon(point.x=corr_dat$var1, point.y=corr_dat$var2, 
+                            pol.x=contour.5[[1]], pol.y=contour.5[[2]], 
+                            mode.checked = TRUE)
+  corr_dat$outlier_check <- in_95 == 0
+  #go back to dataframe to obtain df of only outliers
+  out_index <- which(corr_dat$outlier_check)
+  outliers <- ldply(out_index, function(x) corr_dat[x,])
+  outliers$date
+}
+
+cor_coef_multCluster <- function(corr_dat){
+  #corr_dat <- dataframe with date, var1, var2 columns
+  #returns list timestamps for outliers
+  #remove rows with NA
+  corr_dat <- corr_dat[complete.cases(corr_dat),]
+  #new df with no dates
+  corr_dat_noDate <- data.frame(var1 = corr_dat$var1, 
+                                var2 = corr_dat$var2)
+  corr_mat <- data.matrix(corr_dat_noDate)
+  #get KDE
+  fhat <- kde(x=corr_mat)
+  plot(fhat, cont = seq(5, 95, by = 10))
+  contour.5 <- contourLines(x=fhat$eval.points[[1]],y=fhat$eval.points[[2]],
+                            z=fhat$estimate,levels=fhat$cont["5%"])
+  contour_poly <- list(list())
+  in_95 <- list(list())
+  for(i in seq(1, length(contour.5))){
+    contour_poly[[i]]<- contour.5[[i]]
+    points(contour.5[[i]])
+    in_95[[i]] <- point.in.polygon(point.x=corr_dat$var1, point.y=corr_dat$var2, 
+                                   pol.x=contour.5[[i]]$x, pol.y=contour.5[[i]]$y, 
+                                   mode.checked = TRUE)
+  }
+  #finding points outside of 95 line (have value 0)
+  corr_dat$outlier_check <- TRUE
+  for(i in seq(1, length(in_95))){
+    corr_dat$outlier_check <- (in_95[[i]] == 0 & corr_dat$outlier_check)
+  }
+  #go back to dataframe to obtain df of only outliers
+  out_index <- which(corr_dat$outlier_check)
+  outliers <- ldply(out_index, function(x) corr_dat[x,])
+  outlier_count <- data.frame(dateTimeUTC=outliers$date,
+                              count=1)
+  outliers$date
+}
+
+corr_dat$outlier_check <- NULL
+
+DF <- data.frame(a = 1:10, b = 21:30, c = letters[1:10])
+replace(DF, 1:2, rollapply(DF[1:2], 3, sum, fill = NA))
+rollapply(corr_dat, 47030, by.column=FALSE, FUN=function(x){
+  new <- data.frame(date=as.POSIXct(x[,1], tz='UTC'),
+                    var1=as.numeric(x[,2]),
+                    var2=as.numeric(x[,3]))
+  outliers = cor_coef_multCluster(new)
+  print(outliers)
+})
--- obtaining_clean_data.py
+++ obtaining_clean_data.py
@@ -3,3 +3,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as pltdates
 import datetime as dt
+from main_functions import readFile
+import csv
+
+nc_eno_temp = readFile("NC_Eno_WaterTemp_C.csv")
+nc_eno_DO = readFile("NC_Eno_DO_mgL.csv")
+nc_eno_disch = readFile("NC_Eno_Discharge_m3s.csv")
+
+df_st_temp = nc_eno_temp['dateTimeUTC'].loc[nc_eno_temp['dateTimeUTC']=='2016-07-11 11:45:00'].index.values.astype(int)
+print(df_st_temp)
+df_end_temp = nc_eno_temp['dateTimeUTC'].loc[nc_eno_temp['dateTimeUTC']=='2017-10-20 18:45:00'].index.values.astype(int)
+print(df_end_temp[0])
+test_temp = nc_eno_temp.iloc[60000:df_end_temp[0]]
+test_temp.to_csv('duke_dataplus/' + 'outlier_detect_test_temp' + '.csv')
+
+df_st_out = nc_eno_DO['dateTimeUTC'].loc[nc_eno_DO['dateTimeUTC']=='2017-05-28 11:45:00'].index.values.astype(int)
+print(df_st_out)
+df_end_out = nc_eno_DO['dateTimeUTC'].loc[nc_eno_DO['dateTimeUTC']=='2017-06-02 18:45:00'].index.values.astype(int)
+print(df_end_out)
+test_out = nc_eno_DO.iloc[df_st_out[0]:df_end_out[0]]
+test_out.to_csv('Dirty Data Log/' + 'Outliers_DO' + '.csv')
+
+df_st_out = nc_eno_DO['dateTimeUTC'].loc[nc_eno_DO['dateTimeUTC']=='2017-05-28 11:45:00'].index.values.astype(int)
+print(df_st_out)
+df_end_out = nc_eno_DO['dateTimeUTC'].loc[nc_eno_DO['dateTimeUTC']=='2017-06-02 18:45:00'].index.values.astype(int)
+print(df_end_out)
+test_out = nc_eno_DO.iloc[df_st_out[0]:df_end_out[0]]
+test_out.to_csv('Dirty Data Log/' + 'Outliers_DO' + '.csv')
